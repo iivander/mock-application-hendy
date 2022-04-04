efb0984e4418f83be645114dcbd3f023504c9927
hivander <hendy@movista.com>
2022-04-04 14:28:36 -0500

    Checkpoint: implementing useReducer for state management

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper";
+import data from '../../assets/data/mission.json';
+import { TripContext, TripProvider } from "../../contexts/TripContext/TripContext";
+import Trip from "../Trip/Trip";
 import './App.css';
-import "swiper/css";
-import "swiper/css/pagination";
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 function App() {
+  const test = useContext(TripContext);
+  console.log(test)
+
   return (
-    <div className="App">
-      <Swiper
-        direction={"vertical"}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </div>
+    // <TripProvider>
+      <div className="App">
+        <Swiper
+          direction={"vertical"}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <Trip />
+          </SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </div>
+    // </TripProvider>
   );
 }
 
---
+++ src/contexts/TripContext/TripContext.js
@@ -0,0 +1,27 @@
+import { createContext, useReducer } from "react";
+
+const initialState = {
+  driver: {},
+  trip: {},
+  vehicle: {},
+  vibe: {},
+}
+
+function reducer(state, action) {
+  return { ...state, ...action };
+}
+
+const TripContext = createContext();
+
+const TripProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state)
+
+  return (
+    <TripContext.Provider value={{ state, dispatch }}>
+      {children}
+    </TripContext.Provider>
+  );
+};
+
+export { TripContext, TripProvider };

f81922312ffb6c7652b166eb27daba0661c69545
hivander <hendy@movista.com>
2022-04-05 09:21:02 -0500

    feat: refactor web view to not use swiper

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,55 @@
 import React, { useContext, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
+import { useMediaQuery } from 'react-responsive';
 import data from '../../assets/data/mission.json';
 import altoLogo from '../../assets/images/Alto_logo.png';
 import { TripContext } from '../../contexts/TripContext/TripContext';
 import TripInformation from '../TripInformation/TripInformation';
-import DriverInformation from "../DriverInformation/DriverInformation";
-import VehicleInformation from "../VehicleInformation/VehicleInformation";
-import VibeInformation from "../VibeInformation/VibeInformation";
-import Footer from "../Footer/Footer";
+import DriverInformation from '../DriverInformation/DriverInformation';
+import VehicleInformation from '../VehicleInformation/VehicleInformation';
+import VibeInformation from '../VibeInformation/VibeInformation';
+import Footer from '../Footer/Footer';
 import './App.scss';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
+const MobileView = () => (
+  <Swiper
+    direction={"vertical"}
+    pagination={{
+      clickable: true,
+    }}
+    modules={[Pagination]}
+  >
+    <SwiperSlide>
+      <TripInformation />
+    </SwiperSlide>
+    <SwiperSlide>
+      <DriverInformation />
+    </SwiperSlide>
+    <SwiperSlide>
+      <VehicleInformation />
+    </SwiperSlide>
+    <SwiperSlide>
+      <VibeInformation />
+    </SwiperSlide>
+  </Swiper>
+);
+
+const WebView = () => (
+  <>
+    <TripInformation />
+    <DriverInformation />
+    <VehicleInformation />
+    <VibeInformation />
+  </>
+)
+
 function App() {
   const { dispatch } = useContext(TripContext);
+  const isMobile = useMediaQuery({ query: `(max-width: 760px)` })
+  console.log(isMobile)
 
   useEffect(() => {
     dispatch(data);
@@ -23,26 +58,9 @@ function App() {
   return (
     <div className="App">
       <img className="alto-logo" src={altoLogo} alt="altoLogo" />
-      <Swiper
-        direction={"vertical"}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-      >
-        <SwiperSlide>
-          <TripInformation />
-        </SwiperSlide>
-        <SwiperSlide>
-          <DriverInformation />
-        </SwiperSlide>
-        <SwiperSlide>
-          <VehicleInformation />
-        </SwiperSlide>
-        <SwiperSlide>
-          <VibeInformation />
-        </SwiperSlide>
-      </Swiper>
+      {isMobile
+        ? <MobileView />
+        : <WebView />}
       <Footer />
     </div>
   );
